cde2eb5e280c76d3c0b7bec5b206a250985ad535
Zien Mohammed Abdelmonaem <78083890+zenab12@users.noreply.github.com>
2023-05-23 20:26:45 +0300

    Update .eslintrc.cjs
    edit eslint to support require and modules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+   env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',

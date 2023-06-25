15c92afd6d974e8ef292afbfdf55383de4385037
RahmaHanafi <rahmahanafi809@gmail.com>
2023-06-25 04:15:05 +0300

    handle design

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -5,7 +5,7 @@ import "./Product.css";
 import Navbar from "../../Components/navbar/Navbar";
 import SingleProduct from "./SingleProduct";
 import FilterMenu from "../../Components/Shared/FilterMenu";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 // import sorryImg from "../../../public/images/community/Feeling sorry-cuate.jpg";
 
 const pageSize = 9;
@@ -171,8 +171,37 @@ function Product({ setItemsInCart }) {
       {/* <Navbar /> */}
       <div className="bg-shop w-full max-sm:h-[30vh] sm:h-[30vh] md:h-[40vh] xl:h-[45vh] bg-no-repeat bg-cover ">
         <div className=" w-full h-full bg-gray-600/30 backdrop-brightness-75 flex flex-col justify-center items-center">
-          <h1 className="text-white text-4xl">Shop</h1>
-          <h3 className="text-white text-xl">Home &gt; Shop</h3>
+          <h1 className="text-white font-semibold text-xl md:text-4xl mb-4 md:mb-6">
+            Shop
+          </h1>
+          <ol className="list-none p-0 inline-flex">
+            <li className="flex items-center">
+              <a
+                href="/"
+                className="text-white hover:text-white flex items-start gap-2"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
+                Home
+              </a>
+            </li>
+            <li className="flex items-center text-white">
+              <span className="mx-2">/</span>
+              <span>Shop</span>
+            </li>
+          </ol>
         </div>
       </div>
       <div className="mt-10 m-auto">

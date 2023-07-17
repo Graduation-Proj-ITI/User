9dc95af4a2be6d2c55ac7b05728012c1d415f500
zenab12 <zenabm@std.mans.edu.eg>
2023-07-17 10:35:46 +0300

    edit home product grid with new links and new photos product

--- src/Components/sections/HomeGridProducts.jsx
+++ src/Components/sections/HomeGridProducts.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import gImg1 from "../../../public/images/grid/1.webp";
-import gImg2 from "../../../public/images/grid/2.webp";
-import gImg3 from "../../../public/images/grid/3.webp";
-import gImg4 from "../../../public/images/grid/4.webp";
-import gImg5 from "../../../public/images/grid/5.webp";
+import gImg1 from "../../../public/images/grid/555.webp";
+import gImg2 from "../../../public/images/grid/4444.webp";
+import gImg3 from "../../../public/images/grid/11.webp";
+import gImg4 from "../../../public/images/grid/444.webp";
+import gImg5 from "../../../public/images/grid/333.webp";
+
 import { Link, Navigate, useNavigate } from "react-router-dom";
 
 const GridProducts = () => {
@@ -21,16 +22,16 @@ const GridProducts = () => {
           />
           <div className="group">
             <button
-              className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 bottom-1/2 left-1/4 "
+              className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 bottom-1/3 left-1/2 "
               onClick={() => {
-                naviagte("/product/details/64977ace04ef60e20778ab81");
+                naviagte("/product/details/649774b1d8fa44facec2a148");
               }}
             >
               <i className="fa-solid fa-circle text-white scale-75"></i>
             </button>
 
-            <Link to={"/product/details/64977ace04ef60e20778ab81"}>
-              <div className="absolute hidden bg-white bottom-24 left-28 cursor-pointer p-3 group-hover:block motion-safe:animate-wiggle">
+            <Link to={"/product/details/649774b1d8fa44facec2a148"}>
+              <div className="absolute hidden bg-white bottom-[8%] left-28 cursor-pointer p-3 group-hover:block motion-safe:animate-wiggle">
                 <div className="flex justify-between ">
                   <p className=" text-orange-800  font-bold text-xs py-1">
                     New
@@ -39,14 +40,14 @@ const GridProducts = () => {
                 </div>
 
                 <p className=" text-primary font-poppins font-medium  text-sm py-1">
-                  Mid-Century Modern...
+                Frink 71'' Upholstered Sofa
                 </p>
 
                 <p className=" text-slate-700  font-poppins text-xs">
-                  Short on time, the exqu...
+                This mid-century modern sofa is...
                 </p>
                 <p className=" font-poppins font-semibold text-sm py-1">
-                  22000 LE
+                28999 LE
                 </p>
               </div>
             </Link>
@@ -58,45 +59,50 @@ const GridProducts = () => {
             className=" h-80 w-full lg:h-full rounded-xl object-cover"
           />
           <div className="group">
-            <button className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 bottom-1/4  left-2/4 ">
+            <button
+             onClick={() => {
+              naviagte("/product/details/64b4601f0fbb8a14d2cd6db6");
+            }}
+            className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 bottom-1/2  left-3/4 ">
               <i className="fa-solid fa-circle text-white scale-75"></i>
             </button>
-
-            <div className="absolute hidden bg-white bottom-24 left-36 cursor-pointer p-3 group-hover:block motion-safe:animate-wiggle">
+            <Link to={"/product/details/64b4601f0fbb8a14d2cd6db6"}>
+            <div className="absolute hidden bg-white bottom-24 left-[50%] cursor-pointer p-3 group-hover:block motion-safe:animate-wiggle">
               <div className="flex justify-between ">
                 <p className=" text-orange-800  font-bold text-xs py-1">New</p>
                 <i className="fa-solid fa-angle-right"></i>
               </div>
 
               <p className=" text-primary font-poppins font-medium  text-sm py-1">
-                Storage Unit
+              Esmund Tufted Velvet...
               </p>
 
               <p className=" text-slate-700  font-poppins text-xs">
-                short description...
+              Arriving as a 2-piece...
               </p>
               <p className=" font-poppins font-semibold text-sm py-1">
-                $ 75.00
+                $ 110 LE
               </p>
             </div>
+            </Link>
           </div>
         </figure>
         <figure className="relative col-span-1 row-span-2 md:col-span-1 md:row-span-2 lg:col-span-1 lg:row-span-2 xl:col-span-1 xl:row-span-2 2xl:col-span-1 2xl:row-span-2">
           <img
-            src={gImg3}
+            src={gImg4}
             className=" h-80 w-full lg:h-full rounded-xl object-cover"
           />
           <div className="group">
             <button
-              className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm  scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 top-1/2 left-1/2 "
+              className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm  scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 top-[47%] left-1/2 "
               onClick={() => {
-                naviagte("/product/details/64977c3804ef60e20778ac27");
+                naviagte("/product/details/64b460a90fbb8a14d2cd6dba");
               }}
             >
               <i className="fa-solid fa-circle text-white scale-75"></i>
             </button>
 
-            <Link to={"/product/details/64977c3804ef60e20778ac27"}>
+            <Link to={"/product/details/64b460a90fbb8a14d2cd6dba"}>
               <div className="absolute hidden bg-white top-1/2 left-1/3  cursor-pointer p-3 group-hover:block motion-safe:animate-wiggle">
                 <div className="flex justify-between ">
                   <p className=" text-orange-800  font-bold text-xs py-1">
@@ -106,35 +112,36 @@ const GridProducts = () => {
                 </div>
 
                 <p className=" text-primary font-poppins font-medium  text-sm py-1">
-                  Dining table
+                Euclid Upholstered Accent...
                 </p>
 
                 <p className=" text-slate-700  font-poppins text-xs">
-                  Calling back to the styli...
+                Add this side chair to your...
                 </p>
                 <p className=" font-poppins font-semibold text-sm py-1">
-                  7000 LE
-                </p>
+                110 LE                </p>
               </div>
             </Link>
           </div>
         </figure>
         <figure className="relative col-span-2 row-span-2 sm:col-span-1 md:col-span-2 md:row-span-2 lg:col-span-2 lg:row-span-2 xl:col-span-2 xl:row-span-2 2xl:col-span-2 2xl:row-span-2">
           <img
-            src={gImg4}
+            src={gImg3}
             className=" h-80 w-full lg:h-full rounded-xl object-cover"
           />
           <div className="group">
             <button
-              className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm  scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 right-1/3 bottom-1/3 "
+              className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm  scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 right-1/4 bottom-[30%] "
               onClick={() => {
-                naviagte("/product/details/64977c8204ef60e20778ac5d");
+                naviagte("/product/details/6497738fd8fa44facec29e1d");
               }}
             >
               <i className="fa-solid fa-circle text-white scale-75"></i>
             </button>
             {/* <Link to={}>
             </Link> */}
+                        <Link to={"/product/details/6497738fd8fa44facec29e1d"}>
+
             <div
               className="absolute hidden bg-white bottom-36 right-36 cursor-pointer p-3 group-hover:block motion-safe:animate-wiggle"
               onClick={() => {
@@ -145,16 +152,18 @@ const GridProducts = () => {
                 <p className=" text-orange-800  font-bold text-xs py-1">New</p>
                 <i className="fa-solid fa-angle-right"></i>
               </div>
+              
               <p className=" text-primary font-poppins font-medium  text-sm py-1">
-                Storage Unit
+              12-pair-shoe-rack
               </p>
               <p className=" text-slate-700  font-poppins text-xs">
-                short description...
+              Boots in the entryway, flats...
               </p>
               <p className=" font-poppins font-semibold text-sm py-1">
-                $ 75.00
+              8000 LE
               </p>
             </div>
+</Link>
           </div>
         </figure>
         <figure className="relative col-span-1 2xl:col-span-1">
@@ -164,13 +173,14 @@ const GridProducts = () => {
           />
           <div className="group">
             <button
-              className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm  scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 bottom-1/3 right-1/3 "
+              className="absolute btn-circle bg-slate-800 bg-opacity-50 btn-sm  scale-100 transition-transform ease-linear 0.5s group-hover:scale-90 bottom-1/2 right-1/2 "
               onClick={() => {
                 naviagte("/product/details/64977c3804ef60e20778ac27");
               }}
             >
               <i className="fa-solid fa-circle text-white scale-75"></i>
             </button>
+            <Link to={"/product/details/64977c3804ef60e20778ac27"}>
 
             <div
               className="absolute hidden bg-white bottom-24 right-36 cursor-pointer p-3 group-hover:block motion-safe:animate-wiggle"
@@ -184,16 +194,16 @@ const GridProducts = () => {
               </div>
 
               <p className=" text-primary font-poppins font-medium  text-sm py-1">
-                Storage Unit
-              </p>
+              Karling dinning...              </p>
 
               <p className=" text-slate-700  font-poppins text-xs">
-                short description...
+              Calling back to the stylish...
               </p>
               <p className=" font-poppins font-semibold text-sm py-1">
-                $ 75.00
+              7000 LE
               </p>
             </div>
+            </Link>
           </div>
         </figure>
       </div>
